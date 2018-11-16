6d0f6519437a88f61dbe4a9b4b25085b0d511f26
Steven Stone <steven@stonecodeproductions.com>
2018-11-15 22:46:45 -0600

    Updating footer copy

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,6 +12,23 @@ const LegalCopy = styled('p')`
 export default () => (
   <React.Fragment>
     <Afterbar />
-    <LegalCopy>Le Legal Info</LegalCopy>
+    <LegalCopy>
+      &copy; {new Date().getFullYear()} Knots and Rows, Inc. Website built and
+      maintained by{' '}
+      <a href="https://stone.codes" target="_blank" rel="noreferer">
+        Stone Code Productions
+      </a>
+      .<br />
+      This site uses cookies to make sure you keep the right cart. All personal
+      data handled by{' '}
+      <a
+        href="https://www.shopify.com/legal/privacy"
+        target="_blank"
+        rel="noreferer"
+      >
+        Shopify
+      </a>
+      .
+    </LegalCopy>
   </React.Fragment>
 );
